2b182ab7bf240949a3739fd5a8cbc767421f0dfc
Andrei Bacin <andreib.pf@gmail.com>
2024-12-03 10:11:09 -0300

    feat: CallToAction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+// Importing sections
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Pricing } from "@/sections/Pricing";
 import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
 
 export default function Home() {
   return (
@@ -14,6 +16,7 @@ export default function Home() {
       <ProductShowcase />
       <Pricing />
       <Testimonials />
+      <CallToAction />
     </>
   );
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,43 @@
+// Importing necessary assets and components
+import Image from "next/image";
+import ArrowRight from "@/assets/arrow-right.svg";
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="overflow-x-clip bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container">
+        <div className="section-heading relative">
+          {/* Section title and description */}
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+          {/* Decorative images */}
+          <Image
+            src={starImage}
+            alt="Star image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src={springImage}
+            alt="Spring image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+          />
+        </div>
+        <div className="mt-10 flex justify-center gap-2">
+          {/* Call to action buttons */}
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
